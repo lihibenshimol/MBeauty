2fc9759dc469fb3c7ad7edfa9727584ba8b30900
Lihi Ben Shimol <lihibs26@gmail.com>
2023-07-26 16:33:15 +0300

    https change

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path')
 app.use(express.static('public'))
 
 const corsOptions = {
-    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:3031'],
+    origin: ['https://127.0.0.1:8080', 'https://localhost:8080', 'https://127.0.0.1:3000', 'https://localhost:3000', 'https://localhost:3031'],
     credentials: true
 }
 app.use(cors(corsOptions))
