e0560645bb64890c6b784c8c5de9282796a58c02
Lihi Ben Shimol <lihibs26@gmail.com>
2023-07-27 11:42:44 +0300

    deployed

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -5,7 +5,9 @@ import { productReducer } from './product-reducer.js'
 
 
 // const { createStore, combineReducers } = Redux
-const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+// const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
+
 
 const rootReducer = combineReducers({
     productModule: productReducer,
